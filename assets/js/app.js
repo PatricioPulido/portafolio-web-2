0e2b9a014cd3d0f5d13cf105125a84ee4f5051c0
PatricioPulido <pato.pulido.parra8@gmail.com>
2025-12-22 23:00:59 -0300

    update portafolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,30 @@ $(function () {
 	});
 
 });
+//Animacion hero para que aparezca despues de cargar la pagina
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.querySelector('.hero');
+  if (hero) setTimeout(() => hero.classList.add('show'), 60);
+});
+
+//Animacion para que los elementos se revelen al hacer scroll en cards about-me
+document.addEventListener("scroll", () => {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const visiblePoint = 120;
+
+    if (elementTop < windowHeight - visiblePoint) {
+      el.classList.add("active");
+    }
+  });
+});
+window.addEventListener("load", () => {
+  document.querySelectorAll(".reveal").forEach(el => {
+    el.classList.add("active");
+  });
+});
+
+
